f52af930244ef6d9ff578271fedd4a266103235e
Yaroslav <yarnik75@gmail.com>
2019-10-25 10:04:46 +0300

    Update script.js
    corrected js-code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 	
-			var quan_sn = 0;
-			var quan_ssp = 0;
-			var quan_sspap = 0;
-			var quan_sdbp = 0;
-			var quan_sdep = 0;
+			let quan_sn = 0;
+			let quan_ssp = 0;
+			let quan_sspap = 0;
+			let quan_sdbp = 0;
+			let quan_sdep = 0;
 	
 		var app = angular.module("registry-app", []);
 		
@@ -15,34 +15,23 @@
 					params: { code: "01130549" } }).then(answer => {
 						$scope.data = answer.data;
 						$scope.beneficiaries = answer.data.oCommonInfoBot.beneficiaries;
-						//console.log(answer.data.oCommonInfoBot.beneficiaries);
-						//console.log(answer.data);
-						
+												
 						for (let i=0; i<answer.data.aListRegistry.length; i++) {
 							let {nID, sID_Registry, sName} = answer.data.aListRegistry[i];if (nID == 36 && sID_Registry == "PublicOffering" && sName=="Публічні пропозиції") {$scope.secondTable =true;
 								break;
 							};
 						}
 				});
-			
-		//	$scope.date_filter = () => {
-		//		//var arr = item.slice(0,10).split('-');
-		//		console.log(item);
-		//		return item;
-		//	};
-			
+								
 			$scope.doSecondQuery = function() {
 				if (!$scope.dataSeconds) {
 					$http({method: 'GET',
 						url: 'https://smida-dev.test.idoc.com.ua/api/registry/getTranslatedModels?sOKPO=01130549' }).then(answer => {
 							$scope.dataSeconds = answer.data;
-							console.log(answer.data);
 						});
 					}
 			};
-			
-			
-			
+									
 			$scope.sortName = () => { $scope.sort_stat_pos = 0; $scope.sort_stock_paper = 0; $scope.sort_begin_public = 0; $scope.sort_end_public = 0;
 				if (quan_sn%2 == 0) {$scope.sort_name = 'sNameOfferent.value'; quan_sn++}
 					else {$scope.sort_name = '-sNameOfferent.value'; quan_sn++};
@@ -71,14 +60,13 @@
 			$scope.today = new Date();
 			
 		});
-		
-		
-		// не корректно работает нижеследующий фильтр, но в ввиду дедлайна оставил как есть
+			
 		app.filter('dateFilter', () => {
             return (filterInput) => {
-                var arr = filterInput.slice(0,10).split('-')
-				if (arr[2] != undefined) return arr[2] + "." + arr[1] + "." + arr[0];
-				}
-            });
-
-		
+				let date = new Date();
+				date.setTime(Date.parse(filterInput));
+				return date.toString() === 'Invalid Date' 
+						? '-' 
+						: `${('0' + date.getDay()).slice(-2)}.${('0' + (date.getMonth()+1)).slice(-2)}.${date.getFullYear()}`;
+			}
+        });
